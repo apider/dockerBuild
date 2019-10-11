dce327c29ae7995151cf4cd83b5bea2c5ea5d86a
apider <43216396+apider@users.noreply.github.com>
2019-10-11 22:27:28 +0200

    Update dockerBuild.sh

--- dockerBuild.sh
+++ dockerBuild.sh
@@ -1,5 +1,5 @@
 if [ -z "$1" ] || [ -z "$2" ] ;then
-	echo Usage: $0 new-work-dir git-repo
+	echo Usage: $0 projectName git-repo
 	exit 1
 fi
 
@@ -9,5 +9,5 @@ git clone "$2" "$1"
 cp Dockerfile requirements.txt $1
 cd $1
 exit 0
-sudo docker build -t flask-python3:v1 .
-sudo docker run --name=flask -it -p 5000:5000 flask-python3:v1
+sudo docker build -t "$1":v1 .
+sudo docker run --name="$1" -it -p 5000:5000 "$1":v1
