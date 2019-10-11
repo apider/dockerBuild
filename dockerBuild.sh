072d09690b23cf33ffb2de75c5433151793cbac8
apider <43216396+apider@users.noreply.github.com>
2019-10-11 22:42:53 +0200

    Update dockerBuild.sh

--- dockerBuild.sh
+++ dockerBuild.sh
@@ -1,13 +1,17 @@
 if [ -z "$1" ] || [ -z "$2" ] ;then
 	echo Usage: $0 projectName git-repo
+	echo Example: $0 test123 git@github.com:xxx/xxx.git
 	exit 1
 fi
 
+echo Removing container $1
+sudo docker rm $1
 rm -rf "$1"
 mkdir $1
 git clone "$2" "$1"
 cp Dockerfile requirements.txt $1
 cd $1
-exit 0
 sudo docker build -t "$1":v1 .
 sudo docker run --name="$1" -it -p 5000:5000 "$1":v1
+echo ""; echo "To start container in background:"
+echo sudo docker run --name="$1" -d -p 5000:5000 "$1":v1
