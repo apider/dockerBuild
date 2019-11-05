90221f84419d16e75c438440e405eb122d721051
apider <43216396+apider@users.noreply.github.com>
2019-11-05 22:53:45 +0100

    Update dockerBuild.sh

--- dockerBuild.sh
+++ dockerBuild.sh
@@ -4,15 +4,17 @@ if [ -z "$1" ] || [ -z "$2" ] ;then
 	exit 1
 fi
 
-echo Removing container $1
+echo - Removing container $1
+sudo docker stop $1
 sudo docker rm $1
+echo - Removing build dir...
 rm -rf "$1"
 mkdir $1
 git clone "$2" "$1"
 cp Dockerfile $1
 cd $1
-sudo docker build -t "$1":v1 .
-sudo docker run --name="$1" -it -p 5000:5000 "$1":v1
+sudo docker build -t "$1" .
+sudo docker run --name="$1" -it -p 5000:5000 "$1"
 sudo docker rm $1
 echo ""; echo "To start container in background:"
-echo sudo docker run --name="$1" -d -p 5000:5000 "$1":v1
+echo sudo docker run --restart=always --name="$1" -d -p 5000:5000 "$1"
