4631dc497a03043568caee9393d89d9bc77d0726
filip <filipk73@gmail.com>
2021-03-30 20:17:27 +0200

    kjhsad

--- deploy_port_registry.sh
+++ deploy_port_registry.sh
@@ -7,10 +7,19 @@ fi
 echo --- Stopping and Removing old container with same name: $1
 docker stop $1
 docker rm $1
-docker rmi registry.home:6000/$1
+docker rmi $1
+docker rmi registry.home:6000/"$1"
 echo --- Building container: $1
-docker build --rm -t registry.home:6000/"$1" $3
+docker build --rm -t "$1" $3
+echo docker tag "$1" registry.home:6000/"$1"
+docker tag "$1" registry.home:6000/"$1"
+echo Pushing...
+echo docker push registry.home:6000/"$1"
+docker push registry.home:6000/"$1"
+docker rmi "$1"
+docker rmi registry.home:6000/"$1"
 echo --- Running container: $1
+echo docker run --restart=always --name="$1" -d -p $2:5001 registry.home:6000/"$1"
 docker run --restart=always --name="$1" -d -p $2:5001 registry.home:6000/"$1"
 echo --- Container $1 started.
 docker ps | grep $1
