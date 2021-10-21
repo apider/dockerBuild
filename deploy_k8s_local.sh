74cfa4d7a1d6d97d3db8f40c7024fc97beab3dd2
Fille <filipk73@gmail.com>
2021-10-21 22:13:55 +0200

    nas registry

--- deploy_k8s_local.sh
+++ deploy_k8s_local.sh
@@ -5,11 +5,11 @@ if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] || [ -z "$4" ];then
 fi
 
 echo --- Building container: $1
-docker build --rm -t registry.home:6000/$1:$4 $2
+docker build --rm -t localhost:32000/$1:$4 $2
 echo --- Pushing...
-docker push registry.home:6000/$1:$4
+docker push localhost:32000/$1:$4
 echo --- Removing local image
-docker rmi registry.home:6000/$1:$4
+docker rmi localhost:32000/$1:$4
 
 echo --- Cloning repo: $2
 git clone $2
@@ -17,14 +17,14 @@ git clone $2
 echo --- Re/starting deployment: $1 from manifest.yaml
 #kubectl delete -f $1/manifest.yaml
 kubectl apply -f $1/manifest.yaml
-kubectl set image deployments/$1 $1=registry.home:6000/$1:$4
+kubectl set image deployments/$1 $1=localhost:32000/$1:$4
 echo --- Removing repo: $2
 rm -rf $1
 #echo --- kubectl rollout -n $3 restart deployment/$1
 #kubectl rollout -n $3 restart deployment/$1
 #kubectl rolling-update -n $3 $1
 #kubectl get all -n $3
-echo Deployed: $1, ns: $3, image: $1:$4
 echo ""
-echo To revert to previous run: kubectl set image deployments/$1 $1=registry.home:6000/$1:"<previous-version>"
+echo Deployed: $1, ns: $3, image: $1:$4
+echo To revert to previous run: kubectl set image deployments/$1 $1=localhost:32000/$1:"<previous-version>"
 echo --- Done
