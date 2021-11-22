93e222f5cb20bdc627d70fd23185fda90e15e8c0
Fille <filipk73@gmail.com>
2021-11-22 16:32:22 +0100

    .

--- deploy_k8s.sh
+++ deploy_k8s.sh
@@ -1,3 +1,16 @@
+if [ -z "$1" ] || [ -z "$2" ] ;then
+        # echo Usage: $0 deploymentName git-repo nameSpaceName version
+        echo Usage: $0 git-repo nameSpaceName
+        # echo Example: $0 test123 git@github.com:xxx/xxx.git dev v1
+        echo Example: $0 git@github.com:xxx/xxx.git my-namespace
+        exit 1
+fi
+
+NAME=`echo $1 | awk 'match($0,/\/(.+)\.git/,a){print a[1]}'`
+
+echo --- Building container: $NAME
+docker build --rm -t registry.home:6000/$NAME:latest $1
+echo --- Pushing...
 docker push registry.home:6000/$NAME:latest
 echo --- Removing local image
 docker rmi registry.home:6000/$NAME:latest
@@ -6,11 +19,13 @@ echo --- Cloning repo: $1
 git clone $1
 
 echo --- Re/starting deployment: $1 from manifest.yaml
-kubectl apply -f $1/manifest.yaml
+#kubectl delete -f $1/manifest.yaml
+kubectl apply -f $NAME/manifest.yaml
+# kubectl set -n $3 image deployments/$1 $1=registry.home:6000/$1:latest
 kubectl rollout -n $2 restart deployment/$NAME
 #kubectl rolling-update -n $3 $1
 echo --- Removing local repo: $1
-rm -rf $1
+rm -rf $NAME
 echo Deployed: $NAME, ns: $2, image: $NAME:latest
 echo ""
 echo To revert to previous run: kubectl set -n $2 image deployments/$NAME $NAME=registry.home:6000/$1:"<previous-version>"
