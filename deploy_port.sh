2f2da00d294a8464c09c0bdecc14ae649f816467
filip <filipk73@gmail.com>
2022-04-03 17:13:23 +0200

    port

--- deploy_port.sh
+++ deploy_port.sh
@@ -2,7 +2,7 @@
 
 if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] ;then
 	echo Usage: $0 projectName expose-port git-repo
-	echo Example: $0 test123 5000 git@github.com:xxx/xxx.git
+	echo Example: $0 test123 8080 git@github.com:xxx/xxx.git
 	exit 1
 fi
 
@@ -13,8 +13,8 @@ docker rmi $1
 echo --- Building container: $1
 docker build --rm -t "$1" $3
 echo --- Running container: $1
-echo docker run --restart=always --name="$1" -d -p $2:5001 "$1"
-docker run --restart=always --name="$1" -d -p $2:5001 "$1"
+echo docker run --restart=always --name="$1" -d -p $2:80 "$1"
+docker run --restart=always --name="$1" -d -p $2:80 "$1"
 echo --- Container $1 started.
 docker ps | grep $1
 echo --- Done
